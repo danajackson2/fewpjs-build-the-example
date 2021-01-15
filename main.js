a7e2e26751718ac28f43ebc628d84d0528ac98fe
danajackson2 <danajackson2@gmail.com>
2021-01-15 11:25:58 -0800

    commit tree 1f4b0adc69f773e496e4fb7e26a8b7cdb0703b98

--- main.js
+++ main.js
@@ -2,10 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+mimicServerCall()
+.then(function(){
 
+  addLikeListeners()
 
+  function toggleHeart(e){
+    if (e.target.innerHTML == EMPTY_HEART){
+      e.target.innerHTML = FULL_HEART
+      e.target.className = "activated-heart"
+    } else{
+      e.target.innerHTML = EMPTY_HEART
+      e.target.className = ""
+    }
+  }
 
+  function addLikeListeners(){
+    let hearts = document.getElementsByClassName('like-glyph')
+    Array.from(hearts).forEach(item => item.addEventListener('click', e => toggleHeart(e)))
+  }
+})
+.catch(function(error){
+  console.log(error.message)
+  document.getElementById('modal').className = ""
+  function hide(){
+    (document.getElementById('modal').className = "hidden")
+  }
+  window.setTimeout(hide, 5000)
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
